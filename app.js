82e7ef9cdaa7da0c885bc483407329263ed386e7
Vincent Testagrossa <lancevancevt@gmail.com>
2023-10-29 15:34:07 -0400

    Added buttons for player choices of rps within the content div. Added a results div with paragraphs that display each portion of the result for each round. Switched from alert-based output to changing the textContent of each paragraph and now have continuous games tied into the 'click' events of the buttons.

--- app.js
+++ app.js
@@ -1,63 +1,75 @@
 
-function getPlayerChoice(){
-    parsed = false;
-    while (!parsed){
-        let playerChoice = prompt("Enter your choice: 'rock', 'paper', or 'scissors'.");
-        if (playerChoice === null){
-            parsed = true;
-            return 1;
-        }
-        else if (playerChoice.toLowerCase() == 'rock'){
-            parsed = true;
-            return 1;
-        }
-        else if (playerChoice.toLowerCase() == 'paper'){
-            parsed = true;
-            return 2;
-        }
-        else if (playerChoice.toLowerCase() == 'scissors'){
-            parsed = true;
-            return 3;
-        }
-        else {
-            parsed = false;
-            alert("Invalid Selection.");
-        }
-    }
-}
+const content = document.getElementById('main');
+const results = document.getElementById('results');
+
+const resultsTxt = document.createElement('p');
+const choiceTxt = document.createElement('p');
+const winLoss = document.createElement('p');
+
+const rockBtn = document.createElement('button');
+const paperBtn = document.createElement('button');
+const scissorsBtn = document.createElement('button');
+
+let cpuWins = 0;
+let playerWins = 0;
+let cpuChoice = '';
+let playerChoice = '';
+
+resultsTxt.textContent = `CPU: ${cpuWins} Player: ${playerWins}`;
+choiceTxt.textContent = ``;
+
+rockBtn.textContent = 'Rock';
+rockBtn.value = '1';
+paperBtn.textContent = 'Paper';
+paperBtn.value = '2';
+scissorsBtn.textContent = 'Scissors';
+scissorsBtn.value = '3';
+
+rockBtn.addEventListener('click', (event) => {playRound(event.target.value, getComputerChoice())});
+paperBtn.addEventListener('click', (event) => {playRound(event.target.value, getComputerChoice())});
+scissorsBtn.addEventListener('click', (event) => {playRound(event.target.value, getComputerChoice())});
+
+content.appendChild(rockBtn);
+content.appendChild(paperBtn);
+content.appendChild(scissorsBtn);
+
+results.appendChild(resultsTxt);
+results.appendChild(choiceTxt);
+results.appendChild(winLoss);
+
 function getComputerChoice(){
-    return 1 + Math.floor(Math.random() * 3);
+    return (1 + Math.floor(Math.random() * 3)).toString();
 }
 function getChoiceString(choice){
     switch(choice){
-        case 1:
-            return 'rock';
-        case 2:
-            return 'paper';
-        case 3:
-            return 'scissors';
+        case '1':
+            return 'Rock';
+        case '2':
+            return 'Paper';
+        case '3':
+            return 'Scissors';
     }
     alert(`Something went wrong: choice ${choice} was invalid.`);
     return 'null';
 }
-function playRound(playerChoice, computerChoice){
-    if (computerChoice === playerChoice){
+function findWinner(p1Choice, computerChoice){    
+    if (computerChoice === p1Choice){
         return 0;
     }
     else {
         switch(computerChoice){
-            case 1:
-                if (playerChoice === 2){
+            case '1':
+                if (p1Choice === '2'){
                     return 1;
                 }
                 return -1;
-            case 2:
-                if (playerChoice === 3){
+            case '2':
+                if (p1Choice === '3'){
                     return 1;
                 }
                 return -1;
-            case 3:
-                if (playerChoice === 1){
+            case '3':
+                if (p1Choice === '1'){
                     return 1;
                 }
                 return -1;
@@ -65,44 +77,26 @@ function playRound(playerChoice, computerChoice){
     }
     return -1;
 }
-function game(){
-    let playerScore = 0;
-    let cpuScore = 0;
-    for (let i = 0; i < 5; i++){
-        let computerChoice = getComputerChoice();
-        let cpuString = getChoiceString(computerChoice);
-
-        let playerChoice = getPlayerChoice();
-        let playerString = getChoiceString(playerChoice);
-        if (playRound(playerChoice, computerChoice) === 0){
-            playerScore++;
-            cpuScore++;
-            alert(`It's a tie! ${cpuString} and ${playerString} are the same!`);
-        }
-        else if (playRound(playerChoice, computerChoice) === 1){
-            playerScore++;
-            alert(`You win! ${playerString} beats ${cpuString}.`)
-        }
-        else if (playRound(playerChoice, computerChoice) === -1){
-            cpuScore++;
-            alert(`You lose! ${cpuString} beats ${playerString}.`)
-        }
-        else {
-            alert(`Error: invalid round result.`);
+function playRound(p1Choice, computerChoice){
+    playerChoice = getChoiceString(p1Choice);
+    cpuChoice = getChoiceString(computerChoice);
+    let winner = findWinner(p1Choice, computerChoice);
+    let winTxt = ''
+    switch(winner){
+        case -1:
+            winTxt = 'You lose!';
+            cpuWins++;
             break;
-        }
+        case 0:
+            winTxt = "It's a tie!";
+            break;
+        case 1:
+            winTxt = "YOU WIN!";
+            playerWins++;
+            break;
     }
-    findWinner(playerScore, cpuScore);
+    resultsTxt.textContent = `CPU: ${cpuWins} Player: ${playerWins}`;
+    choiceTxt.textContent = `CPU chose: ${cpuChoice}. You chose ${playerChoice}.`;
+    winLoss.textContent = `${winTxt}`;
+
 }
-function findWinner(playerScore, cpuScore){
-    if (playerScore > cpuScore){
-        alert(`You win! Your score was ${playerScore} and your opponent's was ${cpuScore}.`)
-    }
-    else if (playerScore < cpuScore){
-        alert(`You lose! Your score was ${playerScore} and your opponent's was ${cpuScore}.`)
-    }
-    else{
-        alert(`You tied! Your score was ${playerScore} and your opponent's was ${cpuScore}.`)
-    }
-}
-game();
